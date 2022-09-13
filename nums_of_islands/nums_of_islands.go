e429ffd63cf7f50745b52e388aae2deaf6c724c6
liangnian <liangnian@baidu.com>
2022-09-14 00:29:54 +0800

    岛屿问题优化

--- nums_of_islands/nums_of_islands.go
+++ nums_of_islands/nums_of_islands.go
@@ -80,6 +80,110 @@ func numIslands(grid [][]byte) int {
 	return num
 }
 
+//官方的解法跟我的解法差不多，只不过没有用map存储已经访问过的陆地节点，而是直接标记为水，试试看这种解法是否会超时
+//感觉不是因为这个原因超时的，可能是因为go里面用的list来模拟队列这种实在太重了
+func numIslandsV2(grid [][]byte) int {
+	num := 0
+	if grid == nil {
+		return num
+	}
+	//开始遍历，每次都是BFS遍历
+	row := len(grid)
+	column := len(grid[0])
+
+	for i := 0; i < row; i++ {
+		for j := 0; j < column; j++ {
+
+			//如果是水
+			if grid[i][j] == '0' {
+				continue
+			}
+			l := list.New()
+			l.PushBack(land{i, j})
+			num++
+			for l.Len() != 0 {
+				node := l.Front()
+				//上下左右都需要去判断
+				currnetRow := node.Value.(land).row
+				currnetColumn := node.Value.(land).column
+				//标记为遍历过
+				grid[currnetRow][currnetColumn] = '0'
+
+				//上面
+				if currnetRow-1 >= 0 {
+					if grid[currnetRow-1][currnetColumn] == '1' {
+						l.PushBack(land{currnetRow - 1, currnetColumn})
+					}
+				}
+				//下面
+				if currnetRow+1 < row {
+					if grid[currnetRow+1][currnetColumn] == '1' {
+						l.PushBack(land{currnetRow + 1, currnetColumn})
+					}
+				}
+				//左边
+				if currnetColumn-1 >= 0 {
+					if grid[currnetRow][currnetColumn-1] == '1' {
+						l.PushBack(land{currnetRow, currnetColumn - 1})
+					}
+				}
+				//右边
+				if currnetColumn+1 < column {
+					if grid[currnetRow][currnetColumn+1] == '1' {
+						l.PushBack(land{currnetRow, currnetColumn + 1})
+					}
+				}
+				l.Remove(node)
+			}
+		}
+	}
+
+	return num
+}
+
+//尝试采用DFS遍历
+//DFS这种方式确实不会超时，不过我觉得可能是调用go的list创建代价太高了
+func numIslandsV3(grid [][]byte) int {
+	if grid == nil {
+		return 0
+	}
+	nums := 0
+	//开始遍历，每次都是BFS遍历
+	row := len(grid)
+	column := len(grid[0])
+	var dfs func(i int, j int)
+	dfs = func(i, j int) {
+		if grid[i][j] == '0' {
+			//如果已经是水，则结束
+			return
+		}
+		grid[i][j] = '0'
+		//遍历上下左右
+		if i-1 >= 0 && grid[i-1][j] == '1' {
+			dfs(i-1, j)
+		}
+		if i+1 < row && grid[i+1][j] == '1' {
+			dfs(i+1, j)
+		}
+		if j-1 >= 0 && grid[i][j-1] == '1' {
+			dfs(i, j-1)
+		}
+		if j+1 < column && grid[i][j+1] == '1' {
+			dfs(i, j+1)
+		}
+	}
+
+	for i := 0; i < row; i++ {
+		for j := 0; j < column; j++ {
+			if grid[i][j] == '1' {
+				nums++
+				dfs(i, j)
+			}
+		}
+	}
+	return nums
+}
+
 type land struct {
 	row    int
 	column int
--- nums_of_islands/nums_of_islands_test.go
+++ nums_of_islands/nums_of_islands_test.go
@@ -2,6 +2,8 @@ package leetcode
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNumsOfIslands(t *testing.T) {
@@ -16,6 +18,12 @@ func TestNumsOfIslands(t *testing.T) {
 		t.Error(numIslands(grid))
 	}
 
+	// if numIslandsV2(grid) != 1 {
+	// 	t.Error(numIslandsV2(grid))
+	// }
+
+	assert.Equal(t, numIslandsV3(grid), 1)
+
 	//测试用例2
 	grid2 := [][]byte{
 		{'1', '1', '0', '0', '0'},
@@ -27,11 +35,22 @@ func TestNumsOfIslands(t *testing.T) {
 	if numIslands(grid2) != 3 {
 		t.Error(numIslands(grid2))
 	}
+	// if numIslandsV2(grid2) != 3 {
+	// 	t.Error(numIslandsV2(grid2))
+	// }
+
+	assert.Equal(t, numIslandsV3(grid2), 3)
 
 	//测试用例3
 	grid3 := [][]byte{{'1', '1', '1'}, {'0', '1', '0'}, {'1', '1', '1'}}
 	if numIslands(grid3) != 1 {
 		t.Error(numIslands(grid3))
 	}
+
+	// if numIslandsV2(grid3) != 1 {
+	// 	t.Error(numIslandsV2(grid2))
+	// }
+
+	assert.Equal(t, numIslandsV3(grid3), 1)
 
 }

f5d3de3ba23870409ea04534654a849bfaeee605
liangnian <liangnian@baidu.com>
2022-09-17 10:58:40 +0800

    全排列，先临时提交这种全局变量的写法 leetcode 的AC真的是有问题啊

---
+++ permute/permute.go
@@ -0,0 +1,25 @@
+package leetcode
+
+//https://leetcode.cn/problems/permutations/
+//全排列，其实就是回溯，这块一直也没有吃透
+//对于一些传值与传引用的地方也是没有完全理解清楚
+var res = make([][]int, 0)
+
+func permute(nums []int) [][]int {
+	backtrace(nums, 0, len(nums))
+	return res
+}
+
+//回溯法
+func backtrace(nums []int, i int, length int) {
+	if i == length {
+		newNum := append([]int{}, nums...)
+		res = append(res, newNum)
+	}
+	for j := i; j < length; j++ {
+		nums[i], nums[j] = nums[j], nums[i]
+		backtrace(nums, i+1, length)
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+
+}
---
+++ permute/permute_test.go
@@ -0,0 +1,11 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPermute(t *testing.T) {
+	arr := []int{1, 2, 3}
+	fmt.Println(permute(arr))
+}

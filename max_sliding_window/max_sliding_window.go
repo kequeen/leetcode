7c19e0ffa5cb5dbe38be422fb832f3ccbc675d4f
liangnian <liangnian@baidu.com>
2022-09-22 09:46:26 +0800

    滑动窗口的最大值

--- max_sliding_window/max_sliding_window.go
+++ max_sliding_window/max_sliding_window.go
@@ -23,3 +23,76 @@ func maxSlidingWindow(nums []int, k int) []int {
 	}
 	return result
 }
+
+//优化版解法，一般优化其实核心还是在于减少重复计算
+func maxSlidingWindowV2(nums []int, k int) []int {
+	numsLen := len(nums)
+	if numsLen < k {
+		return []int{}
+	}
+	if k == 1 {
+		return nums
+	}
+	maxNum := nums[0]
+	result := make([]int, 0)
+	for i := 0; i < numsLen; i++ {
+		if maxNum < nums[i] {
+			maxNum = nums[i]
+		}
+		if i < k-1 {
+			continue
+		}
+		result = append(result, maxNum)
+		//判断最前面一位是否最大值
+		if maxNum == nums[i-k+1] {
+			//需要重新选一个最大值
+			maxNum = nums[i-k+2]
+			for j := i - k + 3; j <= i; j++ {
+				if maxNum < nums[j] {
+					maxNum = nums[j]
+				}
+			}
+		}
+	}
+	return result
+}
+
+//其实可以用单调栈的方式来进行解决
+//看了半天才突然间想明白为什么要存储下标,因为一直变小的话，其实没有办法只根据大小判断来退栈
+//所以需要存储下标来兜底
+func maxSlidingWindowV3(nums []int, k int) []int {
+	numsLen := len(nums)
+	if numsLen < k {
+		return []int{}
+	}
+	if k == 1 {
+		return nums
+	}
+	result := make([]int, 0)
+	//构造单调栈
+	stack := []int{}
+	push := func(i int) {
+		//如果大于最初的值，那么就可以将栈底的值去掉
+		for len(stack) > 0 && nums[i] >= nums[stack[len(stack)-1]] {
+			stack = stack[0 : len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+
+	for j := 0; j < k; j++ {
+		push(j)
+	}
+
+	result = append(result, nums[stack[0]])
+
+	for j := k; j < numsLen; j++ {
+		push(j)
+		//判断下是否已经超了当前窗口
+		for j-k >= stack[0] {
+			stack = stack[1:]
+		}
+
+		result = append(result, nums[stack[0]])
+	}
+	return result
+}
--- max_sliding_window/max_sliding_window_test.go
+++ max_sliding_window/max_sliding_window_test.go
@@ -8,4 +8,9 @@ import (
 func TestMaxSlidingWindow(t *testing.T) {
 	arr := []int{2, 3, 4, 1, 3, 2, 7}
 	fmt.Println(maxSlidingWindow(arr, 2))
+	fmt.Println(maxSlidingWindowV2(arr, 2))
+	fmt.Println(maxSlidingWindowV3(arr, 2))
+
+	arr1 := []int{1, 3, 1, 2, 0, 5}
+	fmt.Println(maxSlidingWindowV3(arr1, 3))
 }

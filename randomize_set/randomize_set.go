3eeeb4c84ca3780dd124d5ea500f4346ab69a85f
liangnian <liangnian@baidu.com>
2022-09-24 16:55:00 +0800

    O(1) 时间插入、删除和获取随机元素

--- randomize_set/randomize_set.go
+++ randomize_set/randomize_set.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "math/rand"
+
 //https://leetcode.cn/leetbook/read/top-interview-questions/xagm3s/
 // 实现RandomizedSet 类：
 
@@ -10,39 +12,52 @@ package leetcode
 // 你必须实现类的所有函数，并满足每个函数的 平均 时间复杂度为 O(1) 。
 
 //一般像这种时间复杂度要求O(1)的，其实基本上就只能用map了
+//但其实没有想到最后一个条件，其实用纯map的话，是无法做到每个元素有相同的概率被返回的
+//所以其实就不可能只用一个map的数据结构，还需要一个切片数组，来支持这个的动态扩展
+
+//还有一点看着解法比较奇妙的是，最终删除元素是使用替换的方式，而不是我自己想的拼接数组的方式，不过确实更为合理
 
 type RandomizedSet struct {
-	set map[int]bool
+	set map[int]int
+	arr []int
 }
 
 func Constructor() RandomizedSet {
 	return RandomizedSet{
-		set: make(map[int]bool),
+		set: make(map[int]int),
+		arr: []int{},
 	}
 }
 
 func (this *RandomizedSet) Insert(val int) bool {
-	if this.set[val] {
+	_, ok := this.set[val]
+	if ok {
 		return false
 	}
-	this.set[val] = true
+	this.arr = append(this.arr, val)
+	//存储下标
+	this.set[val] = len(this.arr) - 1
 	return true
 }
 
 func (this *RandomizedSet) Remove(val int) bool {
-	if this.set[val] {
-		delete(this.set, val)
-		return true
+	index, ok := this.set[val]
+	if !ok {
+		return false
 	}
-	return false
+	//首先要移除数组中的这个值，最合适的方式其实是拿另外一个值去覆盖，这里使用数组最后一个值进行覆盖
+	last := len(this.arr) - 1
+	this.arr[index] = this.arr[last]
+	//变更下标
+	this.set[this.arr[index]] = index
+	this.arr = this.arr[:last]
+	delete(this.set, val)
+	return true
 }
 
 func (this *RandomizedSet) GetRandom() int {
-	for key := range this.set {
-		return key
-	}
-	//约定一下，异常返回0
-	return 0
+	index := rand.Intn(len(this.arr))
+	return this.arr[index]
 }
 
 /**

ea168ba1c764fa619ecbdfea9dcc73f57a787673
liangnian <liangnian@baidu.com>
2022-09-25 17:33:48 +0800

    翻转二叉树

---
+++ tree/invertTree/inver_tree.go
@@ -0,0 +1,30 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	invert(root)
+	return root
+}
+
+func invert(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	node.Left, node.Right = node.Right, node.Left
+	invert(node.Left)
+	invert(node.Right)
+}

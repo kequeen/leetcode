b0618187a2b592d7a571f1967156547ebfeb7dbf
liangnian <liangnian1155@gmail.com>
2023-05-22 21:54:49 +0800

    两数之和修订版

--- two_sum/tow_sum.go
+++ two_sum/tow_sum.go
@@ -1,7 +1,7 @@
 package leetcode
 
-//https://leetcode.cn/problems/two-sum/
-//最经典的两数之和
+// https://leetcode.cn/problems/two-sum/
+// 最经典的两数之和
 func twoSum(nums []int, target int) []int {
 	result := make([]int, 0)
 	end := false
@@ -21,4 +21,35 @@ func twoSum(nums []int, target int) []int {
 	return result
 }
 
-//其实是可以用map的，自己把问题想复杂了
+// 其实是可以用map的，自己把问题想复杂了
+// 以前为啥连map都没想到呢，这个是最简单的解法
+func twoSumV2(nums []int, target int) []int {
+	targetMap := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		targetNum := target - nums[i]
+		if _, ok := targetMap[targetNum]; ok {
+			return []int{targetMap[targetNum], i}
+		}
+		targetMap[nums[i]] = i
+	}
+	return nil
+}
+
+// 是不是用双指针也可以
+// 事实证明我傻了，这又不是有序数组
+// 只有有序数据才适合用这种方式
+// func twoSumV3(nums []int, target int) []int {
+// 	left := 0
+// 	right := len(nums) - 1
+// 	for left < right {
+// 		targetNum := nums[left] + nums[right]
+// 		if targetNum == target {
+// 			return []int{left, right}
+// 		} else if targetNum < target {
+// 			left++
+// 		} else {
+// 			right--
+// 		}
+// 	}
+// 	return nil
+// }
--- two_sum/two_sum_test.go
+++ two_sum/two_sum_test.go
@@ -1,33 +1,15 @@
 package leetcode
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTwoSum(t *testing.T) {
-
-	//关于二维数组的吐槽
-	arr := make([][]int, 5)
-	fmt.Println(arr)
-	for i := 0; i < 5; i++ {
-		arr[i] = make([]int, 5)
-	}
-	fmt.Println(arr)
-
-	//关于字符串的遍历问题
-	str := "al"
-	strLen := len(str)
-	fmt.Println(strLen)
-	for i := 0; i < strLen; i++ {
-		fmt.Println(str[i])
-	}
-	for _, item := range str {
-		fmt.Println(item)
-	}
-
-	target := 10
-	arr2 := []int{2, 3, 4, 1, 4, 5, 6}
-	fmt.Println(twoSum(arr2, target))
-
+	num := []int{2, 3, 4, 5}
+	target := 6
+	result := twoSum(num, target)
+	assert.Equal(t, result, []int{0, 2})
+	assert.Equal(t, twoSumV2(num, target), []int{0, 2})
 }
